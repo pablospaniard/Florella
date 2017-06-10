1eb297e70bb8650a98cb3f2fe04345d7caf831d5
PabloSpaniard <pablospaniard@gmail.com>
2017-06-10 13:05:23 +0200

    dropdown menu added, mobile menu and desktop menu done

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,17 @@
- $(document).ready(function(){
-      $('.parallax-window').parallax({imageSrc: './resources/img/bg-foto.jpeg'});
+//Parallax Effect 
+$(document).ready(function () {
+    $('.parallax-window').parallax({
+        imageSrc: './resources/img/bg-foto.jpeg'
     });
+});
+
+$(document).ready(function () {
+    $('.dropdown').hover(
+        function () {
+            $(this).children('.sub-menu').slideDown(200);
+        },
+        function () {
+            $(this).children('.sub-menu').slideUp(200);
+        }
+    );
+});
