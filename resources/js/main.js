4306e110c3c1eb7b555cb7b22de47270dabf1206
PabloSpaniard <pablospaniard@gmail.com>
2017-06-12 12:51:33 +0200

    Added Lightbox

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,56 @@
-//Parallax Effect 
-$(document).ready(function () {
-    $('.parallax-window').parallax({
-        imageSrc: './resources/img/bg-foto.jpeg'
-    });
-});
+$(document).ready(function() {
 
-$(document).ready(function () {
-    $('.dropdown').hover(
-        function () {
-            $(this).children('.sub-menu').slideDown(200);
-        },
-        function () {
-            $(this).children('.sub-menu').slideUp(200);
-        }
-    );
+  // Parallax
+  $('.parallax-window').parallax({
+    imageSrc: './resources/img/bg-foto.jpeg'
+  });
+
+  // Dropdown
+  $('.dropdown').hover(
+    function() {
+      $(this).children('.sub-menu').slideDown(200);
+    },
+    function() {
+      $(this).children('.sub-menu').slideUp(200);
+    }
+  );
+
+  // Lightbox
+  $('.lightbox_trigger').click(function(e) {
+
+    //prevent default action (hyperlink)
+    e.preventDefault();
+
+    //Get clicked link href
+    var image_href = $(this).attr("href");
+
+    if ($('#lightbox').length > 0) { // #lightbox exists
+
+      //place href as img src value
+      $('#content').html('<img src="' + image_href + '" />');
+
+      //show lightbox window - you could use .show('fast') for a transition
+      $('#lightbox').show();
+    } else { //#lightbox does not exist - create and insert (runs 1st time only)
+
+      //create HTML markup for lightbox window
+      var lightbox =
+        '<div id="lightbox">' +
+          '<p>Click to close</p>' +
+          '<div id="content">' + //insert clicked link's href into img src
+            '<img src="' + image_href + '" />' +
+          '</div>' +
+        '</div>';
+
+      //insert lightbox HTML into page
+      $('body').append(lightbox);
+    }
+
+    $('#lightbox').click(function() { //must use live, as the lightbox element is inserted into the DOM
+      $('#lightbox').hide();
+    });
+
+  });
+
+
 });
